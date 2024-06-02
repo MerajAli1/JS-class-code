9cb8496bcd35fdedc2698a967fc726149efa5625
MerajAli1 <merajali45425@gmail.com>
2024-06-02 16:16:56 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+// var clickMe = function a() {
+//     alert("Hello World!")
+// }
+
+
+// function getData() {
+//     var email = document.getElementById('email')
+//     var pass = document.getElementById('password')
+//     var emailVal = email.value
+//     var passVal = pass.value
+//     console.log(emailVal,passVal);
+// }
+
+
+// var pTags = document.getElementsByTagName('p')
+
+// console.log(pTags[1].innerHTML);
+// var array = ["One", "Two", "Three"]
+
+// console.log(array[1]);
+
+
+
+// function validation(params) {
+//     var email = document.getElementById("email")
+//     var emailVal = email.value
+//     if (emailVal === "") {
+//         alert("Field is Required")
+//     }
+//     console.log(emailVal);
+// }
+
+// function data() {
+//     var para = document.getElementById('para')
+//     var paraVal = para.value
+
+//     var span = document.getElementById('span')
+//     span.innerHTML = "<ol><li>Meraj</li><li>Ali</li></ol>"
+// }
+
+
+// function getSrc() {
+//     var img = document.getElementById('img')
+//     img.src = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_640.jpg"
+//     console.log(img.src);
+// }
+
+// function getSrc2() {
+//     var img = document.getElementById('img')
+//     img.src = "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"
+//     console.log(img.src);
+// }
+
+
+function stylingON() {
+    var body = document.getElementsByTagName('body')
+    var para = document.getElementById("para")
+    para.className = "xyz"
+    body[0].className = "xyz"
+}
+function stylingOFF() {
+    var body = document.getElementsByTagName('body')
+    var para = document.getElementById("para")
+    para.className = "abc"
+    body[0].className = "abc"
+}
+
+
+
